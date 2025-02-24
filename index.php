0970767c1cb06173a88b02a48a96fbcd836bf813
Андрей Арефьев <99416982+ArefevAndrey@users.noreply.github.com>
2025-02-24 11:49:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
   <span>Ваше имя:</span>
   <input type="text" class="check-label" name="name" maxLength="20" id="nameForm" placeholder="Имя" onkeypress="return NameValidation(event)" required>
   <span>E-mail:</span>
-  <input type="email" id="emailInput" name="email" class="check-label" id="emailForm" placeholder="gmail@gmail.com" onkeypress="return EmailValidation(event)" required>
+  <input type="email" name="email" class="check-label" id="emailForm" placeholder="gmail@gmail.com" onkeypress="return EmailValidation(event)" required>
   <span>Номер телефона:</span>
   <input type="text" class="check-label" name="phone" maxLength="12" id="numberForm"  placeholder="88005553535" onkeypress="return PhoneValidation(event)" required />
   <span>Сообщение:</span>
